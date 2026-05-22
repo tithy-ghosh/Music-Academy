230fab995e5795318405f180506b786d22cb6646
tithy-ghosh <sgt.tithy.717@gmail.com>
2026-05-22 14:08:06 +0600

    Completed the hero section and made it responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
    <main className='min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]'>
-
+    <HeroSection />
    </main>
   );
 }
